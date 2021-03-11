8df0a3161710ee83b3a69176d1194b769efdb142
AnnatarHe <iamhele1994@gmail.com>
2021-03-11 13:32:56 +0800

    perf(build): reduce bundle size

--- release.ps1
+++ release.ps1
@@ -3,7 +3,7 @@ Write-Host "😁 building & uploading"
 $env:GOOS = "linux"
 $env:GOARCH = "amd64"
 
-go build -tags release -o hookman.exe .\main.go
+go build -ldflags="-s -w" -tags release -o hookman.exe .\main.go
 scp .\hookman.exe annatarhe@annatarhe.cn:~/
 
 Write-Host "✨ done"
